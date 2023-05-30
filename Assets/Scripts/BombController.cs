907b0cb95a679d4b1ca2a11242eb0e3de5f5adf9
Thiago Fontoura <thiagoatf@hotmail.com>
2023-05-30 18:23:38 -0300

    BombaLixo

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -59,13 +59,14 @@ public class BombController : MonoBehaviour
         position.y = Mathf.Round(position.y);
 
         Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        explosion.SetActiveRenderer(explosion.start);
-        explosion.DestroyAfter(explosionDuration);
+        explosion.reference(photonView);
+        explosion.SetActiveRendererNetwork(explosion.start);
+        explosion.destroyAfterNetwork(explosionDuration);
 
-        Explode(position, Vector2.up, explosionRadius);
-        Explode(position, Vector2.down, explosionRadius);
-        Explode(position, Vector2.left, explosionRadius);
-        Explode(position, Vector2.right, explosionRadius);
+        ExplodeNetwork(position, Vector2.up, explosionRadius);
+        ExplodeNetwork(position, Vector2.down, explosionRadius);
+        ExplodeNetwork(position, Vector2.left, explosionRadius);
+        ExplodeNetwork(position, Vector2.right, explosionRadius);
 
         
         Destroy(bomb.gameObject);
@@ -75,8 +76,6 @@ public class BombController : MonoBehaviour
     [PunRPC]
     void PlaceBombCoroutine()
     {
-        StartCoroutine(PlaceBomb());
-
         photonView.RPC("PlaceBombNetwork", RpcTarget.All);
     }
 
@@ -98,16 +97,17 @@ public class BombController : MonoBehaviour
 
         if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
         {
-            ClearDestructible(position);
+            ClearDestruibleNetwork(position);
             return;
         }
 
         Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
-        explosion.SetDirection(direction);
-        explosion.DestroyAfter(explosionDuration);
+        explosion.reference(photonView);
+        explosion.SetActiveRendererNetwork(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirectionNetwork(direction);
+        explosion.destroyAfterNetwork(explosionDuration);
 
-        Explode(position, direction, length - 1);
+        ExplodeNetwork(position, direction, length - 1);
     }
 
     void ExplodeNetwork(Vector2 position, Vector2 direction, int length)
@@ -115,7 +115,8 @@ public class BombController : MonoBehaviour
         photonView.RPC("Explode", RpcTarget.All, position, direction, length);
     }
 
-    
+
+    [PunRPC]    
     private void ClearDestructible(Vector2 position)
     {
         Vector3Int cell = destructibleTiles.WorldToCell(position);
@@ -129,12 +130,10 @@ public class BombController : MonoBehaviour
     }
     void ClearDestruibleNetwork(Vector2 position)
     {
-        ClearDestructible(position);
-
         photonView.RPC("ClearDestructible", RpcTarget.All, position);
     }
 
-
+    [PunRPC]
     public void AddBomb()
     {
         bombAmount++;
--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour
@@ -6,22 +7,44 @@ public class Explosion : MonoBehaviour
     public AnimatedSpriteRenderer middle;
     public AnimatedSpriteRenderer end;
 
+    public PhotonView photonView;
+
+    public void reference(PhotonView go)
+    {
+        photonView = go;
+    }
+
+    [PunRPC]
     public void SetActiveRenderer(AnimatedSpriteRenderer renderer)
     {
         start.enabled = renderer == start;
         middle.enabled = renderer == middle;
-        end.enabled = renderer == end;
+        end.enabled = renderer == end;        
     }
 
+    [PunRPC]
     public void SetDirection(Vector2 direction)
     {
         float angle = Mathf.Atan2(direction.y, direction.x);
         transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
 
+    [PunRPC]
     public void DestroyAfter(float seconds)
     {
         Destroy(gameObject, seconds);
     }
+    public void SetActiveRendererNetwork(AnimatedSpriteRenderer renderer)
+    {
+        photonView.RPC("SetActiveRenderer", RpcTarget.All, renderer);
+    }
 
+    public void destroyAfterNetwork(float seconds)
+    {
+        photonView.RPC("DestroyAfter", RpcTarget.All, seconds);
+    }
+    public void SetDirectionNetwork(Vector2 direction)
+    {
+        photonView.RPC("SetDirection", RpcTarget.All, direction);
+    }
 }
