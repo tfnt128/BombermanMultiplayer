f2fad75dc41a174f65feed5cdbea73077f551c30
Thiago Fontoura <thiagoatf@hotmail.com>
2023-06-07 20:16:33 -0300

    AA

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,41 +8,43 @@ public class MovementController : MonoBehaviourPunCallbacks
     private PhotonView _photonView;
 
     public float speed = 5f;
-    
+
     private Vector2 direction = Vector2.down;
     private Rigidbody2D _rigidbody;
-    
+
+    private bool isDead = false;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _photonView = GetComponent<PhotonView>();
     }
-    
+
     private void Update()
     {
-        if (_photonView.IsMine)
+        if (_photonView.IsMine && !isDead)
         {
             float hor = Input.GetAxis("Horizontal");
             float ver = Input.GetAxis("Vertical");
-            SetDirection(new Vector2(hor,ver));
+            SetDirection(new Vector2(hor, ver));
         }
     }
-    
+
     private void FixedUpdate()
     {
-        if (_photonView.IsMine)
+        if (_photonView.IsMine && !isDead)
         {
             Vector2 position = _rigidbody.position;
             Vector2 translation = direction * (speed * Time.fixedDeltaTime);
             _rigidbody.MovePosition(position + translation);
         }
     }
-    
+
     private void SetDirection(Vector2 newDirection)
     {
         direction = newDirection;
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Explosion"))
@@ -51,11 +53,7 @@ public class MovementController : MonoBehaviourPunCallbacks
         }
     }
 
-    // PUN
-    private bool isDead = false;
-
-    [PunRPC]
-    private void DeathSequence()
+    private void Die()
     {
         if (isDead)
         {
@@ -64,13 +62,12 @@ public class MovementController : MonoBehaviourPunCallbacks
 
         isDead = true;
 
-        enabled = false;
-        GetComponent<BombController>().enabled = false;
-
         if (_photonView.IsMine)
         {
-            Invoke(nameof(DeathSequenceEnd), 1.25f);
+            _photonView.RPC("OnDeathSequenceEnded", RpcTarget.All, !_photonView.IsMine);
         }
+
+        gameObject.SetActive(false);
     }
 
     [PunRPC]
@@ -87,17 +84,5 @@ public class MovementController : MonoBehaviourPunCallbacks
                 screenManager.Instance.ShowVictoryScreen();
             }
         }
-
-        gameObject.SetActive(false);
-    }
-
-    private void DeathSequenceEnd()
-    {
-        _photonView.RPC("OnDeathSequenceEnded", RpcTarget.All, !isDead);
-    }
-
-    private void Die()
-    {
-        _photonView.RPC("DeathSequence", RpcTarget.All);
     }
 }
